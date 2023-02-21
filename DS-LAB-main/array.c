96b5da82557db9988419039fdd54dee995b0305b
Usman777z <116332448+Usman777z@users.noreply.github.com>
2023-02-22 03:15:48 +0700

    Add files via upload

---
+++ DS-LAB-main/array.c
@@ -0,0 +1,115 @@
+#include<stdio.h>
+int main()
+{
+    int ar[20],n,i,ch;
+    void insert(int ar[],int n);
+    void delete(int ar[],int n);
+    void display(int ar[],int n);
+    void search(int ar[],int n);
+    printf("enter the size of array :");
+    scanf(" %d",&n);
+    printf("enter the array elements :");
+    for(i=0;i<n;i++)
+    {
+        scanf(" %d",&ar[i]);
+    }
+    printf("menu:\n 1.insert \n 2.delete \n 3.display \n 4.search");
+    printf("\n enter the choice:");
+    scanf("%d",&ch);
+    switch(ch)
+    {
+      case 1:insert(ar,n);
+        break;
+      case 2:delete(ar,n);
+        break;
+      case 3:display(ar,n);
+        break;
+      case 4:search(ar,n);
+        break;
+      default:
+      printf("wrong choice");
+  }
+  }
+  
+  void insert(int ar[],int n)
+  {
+    int pos,e,i;
+    printf("enter the position :");
+    scanf(" %d",&pos);
+    printf("enter the element to be inserted :");
+    scanf(" %d",&e);
+    for(i=n-1;i>=pos-1;i--)
+    {
+        ar[i+1]=ar[i];
+        
+    }
+    ar[pos-1]=e;
+    n=n+1;
+    
+   printf("array after insertion :");
+    for(i=0;i<n;i++)
+    {
+        printf(" %d",ar[i]);
+    }
+  }
+  void delete(int ar[],int n)
+  {
+    int i,pos,e;
+    if(n==0)
+    {
+        printf("array is empty !!");
+    }
+    else
+    {
+       printf("enter a valid position to be deleted :"); 
+       scanf(" %d",&pos);
+    }
+    e=ar[pos-1];
+    printf("deleted element : %d",e);
+    for(i=pos-1;i<n;i++)
+    {
+        ar[i]=ar[i+1];
+    }
+    n=n-1;
+    printf("\n array after deletion :");
+    for(i=0;i<n;i++)
+    {
+        printf(" %d",ar[i]);
+    }
+  }
+  void display(int ar[],int n)
+  {
+    int i;
+    printf("\n display array elements :");
+    for(i=0;i<n;i++)
+    {
+        printf(" %d\t",ar[i]);
+    }
+  }
+  void search(int ar[],int n)
+  {
+      int e,i,pos,flag=0;
+      printf("enter the element to be searched :");
+      scanf(" %d",&e);
+      for(i=0;i<n;i++)
+      {
+        if(e==ar[i])
+        { 
+            flag=1;
+            pos=i+1;
+            break;
+        }
+        
+      }
+      if(flag==1)
+      {
+          printf("element found at position : %d",pos);
+      }
+      else
+      {
+          printf("element not found!!");
+      }
+  }
+  
+ 
+ 
